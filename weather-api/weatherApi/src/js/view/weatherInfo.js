78b6e72e883a819e37c70aced6d44cdc3784b5a8
enricoferreira <38507827+enricoferreira@users.noreply.github.com>
2020-04-27 22:06:22 -0300

    option to run in http-server .

---
+++ weather-api/weatherApi/src/js/view/weatherInfo.js
@@ -0,0 +1,14 @@
+const nameCity = document.querySelector("[data-city-name]");
+const currentTemp = document.querySelector("[data-current-temp");
+const descriptionWeather = document.querySelector("[data-describe]");
+const maxTemperature = document.querySelector('[data-max]');
+const minTemperature = document.querySelector('[data-min]');
+
+function updateWeatherInfo(weather){
+    nameCity.innerHTML = weather.name;
+    currentTemp.innerHTML = weather.main.temp;
+    descriptionWeather.innerHTML = weather.weather[0].description;
+    maxTemperature.innerHTML = weather.main.temp_max;
+    minTemperature.innerHTML = weather.main.temp_min;
+    console.log(weather);
+}
