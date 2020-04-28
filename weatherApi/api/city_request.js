6a9c475f2000d46dce2ef1dbc73902852d3efdc2
Enrico <enricoferreira41@gmail.com>
2020-04-27 22:09:36 -0300

    new folder

--- weatherApi/api/city_request.js
+++
@@ -1,8 +0,0 @@
-const listarCidades = ()=>{
-    return fetch('./api/json/city.json')
-    .then(r => {
-        console.log(r.status);
-        return r.json();
-    })
-    .then(json => json);
-}
